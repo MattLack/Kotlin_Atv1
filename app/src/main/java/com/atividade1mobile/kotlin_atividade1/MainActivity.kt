db9676fef9e3f80f2101359d79d0fb74e42a52dc
Mateus Lackome <a.mateuslol@gmail.com>
2018-10-11 20:49:13 -0300

    refactore

---
+++ app/src/main/java/com/atividade1mobile/kotlin_atividade1/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.atividade1mobile.kotlin_atividade1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+
+    var result: Int? = 0
+
+    fun calculate(view: View?) {
+
+        dicoverBest()
+
+        if (result == R.string.resultAlc)
+            tvResult.text = getString(R.string.resultAlc)
+        else if (result == R.string.resultGas)
+            tvResult.text = getString(R.string.resultGas)
+        else if (result == 0)
+            Toast.makeText(this, getString(R.string.error), Toast.LENGTH_SHORT).show()
+
+    }
+
+    private fun dicoverBest() {
+
+        try {
+
+            var gas = tfGas.text.toString().toDouble()
+            var alc = tfEtanol.text.toString().toDouble()
+
+            when ((gas * 0.7) > alc) {
+                true -> result = R.string.resultAlc
+                false -> result = R.string.resultGas
+            }
+
+        } catch (e: Exception) {
+            result = 0
+        }
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
